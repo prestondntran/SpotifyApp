05ffd9c2098765da46b4502d1624a398781a3330
prestondntran <prestondnt@gmail.com>
2023-08-17 22:51:32 -0700

    Turned on debug

--- spotifyrec/settings.py
+++ spotifyrec/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_14=*i4(0cp1vs4shp+0!0s-4%qfq3vo7()!lk@9q7f%hlj=5b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
